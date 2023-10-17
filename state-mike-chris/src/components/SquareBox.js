94de40cf0dc6b3789a5ca06323c8667243ad9693
Christopher <ckmarshal23@gmail.com>
2023-10-17 14:44:28 -0700

    finished challenge

--- state-mike-chris/src/components/SquareBox.js
+++ state-mike-chris/src/components/SquareBox.js
@@ -1,31 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const SquareBox = () => {
-  const colors = ["red",  "orange", "yellow", "green", "blue", "purple", 
-  "pink"]  
-  const [currentColorIndex, setCurrentColorIndex] = useState(0)
+  const colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
 
   const boxStyle = {
-    width: '200px', // Adjust the width as needed
-    height: '200px', // This maintains a square aspect ratio
+    width: '200px',
+    height: '200px',
     border: '2px solid black',
-colors[currentColorIndex],
-    backgroundColor: 'white',
+    backgroundColor: colors[currentColorIndex], // Fix: Set the background color based on the current color index
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     cursor: 'pointer',
+  };
 
-    const changeColor = () => {
-        setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
-      }
-  }
+  const changeColor = () => {
+    setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+  };
+
   return (
     <div style={boxStyle} onClick={changeColor}>
       <span style={{ color: 'black', fontSize: '20px' }}>{colors[currentColorIndex]}</span>
     </div>
-  )
-}
-
+  );
+};
 
 export default SquareBox;
