2064eb2abe937ec0b37e0aff84976df302794f24
Patsy <patsyesaurer@gmail.com>
2023-10-17 16:31:28 -0700

    worked on React state challenges.

--- state-patsy-jerrod/src/App.js
+++ state-patsy-jerrod/src/App.js
@@ -4,16 +4,18 @@ import ColorBox from './components/ColorBox';
 import ColorBox2 from './components/ColorBox2';
 import ColorBox3 from './components/ColorBox3';
 import ColorBox4 from './components/ColorBox4';
+import AddBoxButton from './components/AddBoxButton';
 
 const App = () => {
   return (
     <>
-    <div id="boxes">
+    <div id="boxes" style= {{display: 'none'}}>
         <ColorBox/>
         <ColorBox2/>
         <ColorBox3/>
         <ColorBox4/>
     </div>
+    <AddBoxButton/>
     </>
   )
 }
