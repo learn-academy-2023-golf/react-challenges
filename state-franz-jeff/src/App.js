c22d08481a1f0299a1a9053e9f3e148770544a54
jeffguzi <jeffreystephano@gmail.com>
2023-10-17 16:29:02 -0700

    adds react State challenges

---
+++ state-franz-jeff/src/App.js
@@ -0,0 +1,12 @@
+import Counter from './components/Counter';
+import './App.css'
+const App = () => {
+  return(
+    <>
+      <h1>Color Box</h1>
+      <h3><Counter /></h3>
+    </>
+  )
+}
+
+export default App;
