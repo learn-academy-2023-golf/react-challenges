b339a8ce7d53e4721c34f5b94c0d4077f55d6b45
jeffguzi <jeffreystephano@gmail.com>
2023-10-19 16:16:17 -0700

    updates code

--- props-markayel-jeffrey/src/App.js
+++ props-markayel-jeffrey/src/App.js
@@ -4,21 +4,23 @@ import PreviousRolls from './components/PreviousRolls';
 import './App.css';
 
 const App = () => {
-  const nums = ["1", "2", "3", "4", "5", "6"]
+  const nums = ["1", "2", "3", "4", "5", "6", "7"]
   const [diceNums, setdiceNums] = useState(nums[0])
-
   const triggerdiceRoll = () =>{
     let nextNum = Math.floor(Math.random() * nums.length)
   setdiceNums(nums[nextNum])
-  
-  const previousRolls  => {(triggerdiceRoll)}
+  console.log(nextNum)}
+  const history = []
+  const [oldNum, setoldNum] = useState(history)
+  const previousroll = () => {
+    let lastRoll = history.push(diceNums)
+    setoldNum(history[lastRoll])
   }
-
   return(
     <>
     <h1> Dice Roll </h1>
-    <Box triggerdiceRoll={triggerdiceRoll} />
-    <PreviousRolls previousRolls={previousRolls} />
+    <Box diceNum={diceNums} triggerdiceRoll={triggerdiceRoll} />
+    <PreviousRolls previousroll={previousroll} oldNums={oldNum}/>
 
     </>
   )
--- props-markayel-jeffrey/src/components/PreviousRolls.js
+++ props-markayel-jeffrey/src/components/PreviousRolls.js
@@ -1,16 +1,13 @@
 import React, {useState} from "react";
 
 const PreviousRolls = (props) => {
-
     
 return (
-    <div className= "Dice" onClick = {props.nums}>
-
+    <div className= "history">
+       <h4>History: {props.oldNums}</h4>
     </div>
 
 )
 }
-
-
 
 export default PreviousRolls
